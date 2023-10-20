9ca1c058df689f1c3fd267c7ebbce95d276da0ab
mur4ik18 <o.o.kotov1999@gmail.com>
2023-10-20 10:28:52 +0200

    Add instruction ADD and Stock in CPU registre

--- main.c
+++ main.c
@@ -34,8 +34,15 @@ void cycle_emulator();
 
 int main(void)
 {
-  memory[4] = 0xAC;
-  memory[5] = 0xF0;
+  // memoir writing
+  memory[0] = 0x61;
+  memory[1] = 0x05;
+
+  memory[2] = 0x62;
+  memory[3] = 0x02;
+  
+  memory[4] = 0x81;
+  memory[5] = 0x24;
 
   // Emulation loop
   for (;;)
@@ -62,40 +69,81 @@ void cycle_emulator()
   switch (opcode & 0xF000) {
     // SYS addr 
   case 0x0000:
+    switch (opcode & 0x00FF) {
+      // RET 
+    case 0x00EE:
+      pc = stack[sp--];
+      printf("return from subrutine\n");
+      break;
+      // clear display
+    case 0x00E0:
+      printf("clear_display()\n");
+      break;
+    }
     break;
     // JP addr
   case 0x1000:
     pc = opcode & 0x0FFF;
     break;
-    // Call addr
+    // Call addr call subrutine
+    // The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
   case 0x2000:
+    stack[sp] = pc;
+    sp++;
+    pc = opcode & 0x0FFF;
     break;
+    // SE Vx byte
   case 0x3000:
     break;
+    // SNE
   case 0x4000:
     break;
+    // SE Vx Vy
   case 0x5000:
     break;
+    // SET NN in Vx
+    // 0x6XNN
   case 0x6000:
+    V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+    printf("%04X <- %d\n", (opcode & 0x0F00) >> 8, V[(opcode & 0x0F00) >> 8]);
     break;
+    //
   case 0x7000:
     break;
+    // 
   case 0x8000:
+    switch (opcode & 0x000F) {
+      // ADD VY to VX
+      // 0x8xy4
+    case 0x0004: 
+      printf("Add VY to VX\n");
+      printf("Add %d to %d\n", V[(opcode & 0x00F0) >> 4],  V[(opcode & 0x0F00) >> 8]);
+      V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
+      printf("%d\n",V[(opcode & 0x0F00) >> 8]);
+      break;
+    }
     break;
+    //
   case 0x9000:
     break;
+    //
   case 0xA000:
     I = opcode & 0x0FFF;
     printf("I = %04X\n", I);
     break;
+    //
   case 0xB000:
     break;
+    //
   case 0xC000:
     break;
+    //
   case 0xD000:
     break;
+    //
   case 0xE000:
     break;
+    //
   case 0xF000:
     break;
     
